f8e6045032c0eb467c26eec1a98ed667ac2dc959
aminul-islam-be <112537464+aminul-islam-be@users.noreply.github.com>
2026-07-30 21:42:12 +0600

    Create Thank You page component

---
+++ app/thank-you/page.js
@@ -0,0 +1,13 @@
+export default function ThankYouPage() {
+  return (
+    <main style={{ padding: "40px", textAlign: "center" }}>
+      <h1>🎉 Thank You!</h1>
+
+      <p>Your payment request has been submitted.</p>
+
+      <p>Your order is now waiting for Admin Approval.</p>
+
+      <a href="/">Continue Shopping</a>
+    </main>
+  );
+}
